217cfcdfe43bd22b9d3ff1a60db424882f528548
alsrhkd77 <alsrhkd77@gmail.com>
2023-04-25 12:41:22 +0900

    auth api 정리
    종속정 주입으로 변경, redirect 줄이기

--- app/api/auth/discord.py
+++ app/api/auth/discord.py
@@ -4,7 +4,7 @@ from requests import Session
 from starlette import status
 
 from app.discord_provider import discord_provider
-from app.api.dependencies import get_db
+from app.api.dependencies import get_db, get_uuid_from_token, get_host_url
 from app.crud.crud_user import crud_user
 from app.schemas.user import UserCreate
 from app.utils.token_factory import create_access_token, validate_access_token
@@ -14,47 +14,38 @@ router = APIRouter(
 )
 
 
-@router.get('/authenticate')
-def authenticate(platform: str, access_token: str, refresh_token: str, db: Session = Depends(get_db)):
-    if platform == 'web':
-        platform_url = f"https://www.karanda.kr/auth/authenticate"
-        # platform_url = f"http://localhost:2345/#/auth/authenticate"
-    if platform == "windows":
-        platform_url = f"http://localhost:8082"
+def authenticate(access_token: str, db: Session):
     user_data = discord_provider.get_user_data(access_token)
     user = crud_user.get_by_discord_id(db, discord_id=user_data['id'])
     if user is None:
         user = crud_user.create(db, obj_in=UserCreate(discord_id=user_data['id']))
     token = create_access_token(uuid=user.uuid)
-    return RedirectResponse(
-        url=f"{platform_url}?token={token}&access_token={access_token}&refresh_token={refresh_token}")
+    return token
 
 
 @router.get('/authenticate/windows')
-def authentication_windows(code: str, request: Request):
-    host_url = str(request.url).split('?')[0]
+def authentication_windows(code: str, host_url: str = Depends(get_host_url), db: Session = Depends(get_db)):
     data = discord_provider.exchange_code(code=code, redirect_url=host_url)
-    redirect_url = f"/auth/discord/authenticate?platform=windows&access_token={data['access_token']}&refresh_token={data['refresh_token']}"
+    token = authenticate(access_token=data['access_token'], db=db)
+    url = f"http://localhost:8082"
+    redirect_url = f"{url}?token={token}&access_token={data['access_token']}&refresh_token={data['refresh_token']}"
     return RedirectResponse(url=redirect_url)
 
 
 @router.get('/authenticate/web')
-def authentication_web(code: str, request: Request):
-    host_url = str(request.url).split('?')[0]
+def authentication_web(code: str, host_url: str = Depends(get_host_url), db: Session = Depends(get_db)):
     data = discord_provider.exchange_code(code=code, redirect_url=host_url)
-    redirect_url = f"/auth/discord/authenticate?platform=web&access_token={data['access_token']}&refresh_token={data['refresh_token']}"
+    token = authenticate(access_token=data['access_token'], db=db)
+    url = f"https://www.karanda.kr/auth/authenticate"
+    # url = f"http://localhost:2345/#/auth/authenticate"
+    redirect_url = f"{url}?token={token}&access_token={data['access_token']}&refresh_token={data['refresh_token']}"
     return RedirectResponse(url=redirect_url)
 
 
 @router.get('/authorization')
-def authorization(access_token: str, request: Request, db: Session = Depends(get_db)):
-    if request.headers.keys().__contains__('authentication'):
-        token = request.headers.get('authentication')
-    elif request.cookies.keys().__contains__('authentication'):
-        token = request.cookies.get('authentication')
-    else:
+def authorization(access_token: str, uuid: str = Depends(get_uuid_from_token), db: Session = Depends(get_db)):
+    if uuid == '':
         return Response(status_code=status.HTTP_401_UNAUTHORIZED)
-    uuid = validate_access_token(token=token)
     data = discord_provider.get_user_data(access_token)
     user = crud_user.get_by_uuid(db, user_uuid=uuid)
     if data['id'] != user.discord_id:
--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,6 +1,9 @@
 from typing import Generator
+from fastapi import Request, Response
+from starlette import status
 
 from app.database.session import SessionLocal
+from app.utils.token_factory import validate_access_token
 
 
 def get_db() -> Generator:
@@ -9,3 +12,17 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
+
+
+def get_uuid_from_token(request: Request):
+    if request.headers.keys().__contains__('authentication'):
+        token = request.headers.get('authentication')
+    elif request.cookies.keys().__contains__('authentication'):
+        token = request.cookies.get('authentication')
+    else:
+        return ''
+    return validate_access_token(token=token)
+
+
+def get_host_url(request: Request):
+    return str(request.url).split('?')[0]
