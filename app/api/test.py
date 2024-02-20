036cdcd880e5e0ca44d5aeb85163a49750bf83b2
alsrhkd77 <alsrhkd77@gmail.com>
2024-02-20 15:02:14 +0900

    test websocket

--- app/api/test.py
+++ app/api/test.py
@@ -1,43 +1,51 @@
+import json
+
 from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
 from starlette.requests import Request
 from starlette.websockets import WebSocket, WebSocketDisconnect
+from app.utils.web_socket_manager import trade_market_websocket_manager
 
 router = APIRouter(prefix='/test')
 
 
 @router.websocket('/echo')
 async def echo(websocket: WebSocket):
-    await websocket.accept()
+    await trade_market_websocket_manager.accept(websocket)
     try:
         while True:
             data = await websocket.receive_text()
-            await websocket.send_text(data)
+            await websocket.send_text(json.dumps(jsonable_encoder({'msg': data})))
+            await trade_market_websocket_manager.broadcast(json.dumps(jsonable_encoder({'msg': data})))
     except WebSocketDisconnect:
         print("disconnect")
 
 
 @router.websocket('/echo-if')
 async def echo_if(websocket: WebSocket):
-    await websocket.accept()
+    await trade_market_websocket_manager.accept(websocket)
     try:
         while True:
             data = await websocket.receive_text()
+            await websocket.send_text(json.dumps(jsonable_encoder({'msg': data})))
             if data == 'update':
-                await websocket.send_text(data)
+                await trade_market_websocket_manager.broadcast(json.dumps(jsonable_encoder({'msg': data})))
     except WebSocketDisconnect:
         print("disconnect")
 
 
 @router.websocket('/welcome')
 async def welcome(websocket: WebSocket):
-    await websocket.accept()
-    await websocket.send_text('welcome')
+    await trade_market_websocket_manager.accept(websocket)
+    await websocket.send_text(json.dumps(jsonable_encoder({'msg': 'welcome'})))
     try:
         while True:
             data = await websocket.receive_text()
-            await websocket.send_text(data)
+            await websocket.send_text(json.dumps(jsonable_encoder({'msg': data})))
+            await trade_market_websocket_manager.broadcast(json.dumps(jsonable_encoder({'msg': data})))
     except WebSocketDisconnect:
         print("disconnect")
+
 
 @router.get('/get/{item}')
 def get_test(request: Request, item: str):
