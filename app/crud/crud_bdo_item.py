e03ac5bd252775a3f7045c3e704ffa1d49a07aa9
alsrhkd77 <alsrhkd77@gmail.com>
2024-02-20 22:34:47 +0900

    update trade-market

--- app/crud/crud_bdo_item.py
+++ app/crud/crud_bdo_item.py
@@ -7,15 +7,15 @@ from app.models import BdoItem
 
 
 class CRUDBdoItem:
-    async def get_tradeable_item_by_item_num(self, db: Session, *, item_num: int) -> Optional[BdoItem]:
+    def get_tradeable_item_by_item_num(self, db: Session, *, item_num: int) -> Optional[BdoItem]:
         return db.query(BdoItem).filter(BdoItem.tradeable == True).filter(BdoItem.item_num == item_num).one_or_none()
 
-    async def get_by_item_name(self, db: Session, *, item_name: str) -> Optional[BdoItem]:
+    def get_by_item_name(self, db: Session, *, item_name: str) -> Optional[BdoItem]:
         if not item_name:
             return None
         return db.query(BdoItem).filter(BdoItem.item_num == item_name).one_or_none()
 
-    async def get_all_tradeable_item_by_item_num(self, db: Session, item_num_list: list) -> Optional[List[BdoItem]]:
+    def get_all_tradeable_item_by_item_num(self, db: Session, item_num_list: list) -> Optional[List[BdoItem]]:
         if not item_num_list:
             return None
         return db.query(BdoItem).filter(BdoItem.tradeable == True).filter(BdoItem.item_num.in_(item_num_list)).all()
