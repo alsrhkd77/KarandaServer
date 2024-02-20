079329bd267dff1e2622a540d540cae402852627
alsrhkd77 <alsrhkd77@gmail.com>
2024-02-20 20:49:06 +0900

    update function async

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -43,6 +43,7 @@ async def get_token_from_websocket(
         token: Annotated[str | None, Query()] = None,
 ) -> Optional[str]:
     print(session)
+
     if token is None:
         raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
     payload = validate_access_token(token=token)
