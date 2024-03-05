3d140294f8bdc26d3951d4c9a46e2e5f1b9b3b59
alsrhkd77 <alsrhkd77@gmail.com>
2024-03-05 22:16:54 +0900

    불필요 코드 제거

--- app/api/trade_market.py
+++ app/api/trade_market.py
@@ -228,11 +228,7 @@ def get_latest(request: Request, target_list: list[str] = Query(None)):
     need_update = []
     # Categorize items that need to be updated
     for key, v in itertools.groupby(db_data, key=lambda x: x.item_num):
-        try:
-            item_num_list.remove(key)
-        except Exception:
-            print(item_num_list)
-            print(key)
+        item_num_list.remove(key)
         value = list(v)
         if value[0].date < now - timedelta(minutes=15):
             need_update = need_update + value
