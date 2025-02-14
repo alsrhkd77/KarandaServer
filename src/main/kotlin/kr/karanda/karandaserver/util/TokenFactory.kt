cc56767d5c8cf2ceb79fa8577ae30c3f0f17a25f
MinGwang Song <alsrhkd77@gmail.com>
2025-02-15 01:53:43 +0900

    코드 정리 및 `createTokens` 함수의 RefreshToken 생성 부분 수정

--- src/main/kotlin/kr/karanda/karandaserver/util/TokenFactory.kt
+++ src/main/kotlin/kr/karanda/karandaserver/util/TokenFactory.kt
@@ -5,13 +5,10 @@ import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.UnsupportedJwtException
 import io.jsonwebtoken.security.MalformedKeyException
 import io.jsonwebtoken.security.SecurityException
-import jakarta.annotation.PostConstruct
 import kr.karanda.karandaserver.data.TokenProperties
 import kr.karanda.karandaserver.data.Tokens
-import kr.karanda.karandaserver.dto.User
-import kr.karanda.karandaserver.service.FireStoreService
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.DependsOn
+import kr.karanda.karandaserver.dto.TokenClaims
+import kr.karanda.karandaserver.repository.DefaultDataRepository
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
@@ -21,32 +18,26 @@ import java.util.Date
 import javax.crypto.spec.SecretKeySpec
 
 @Component
-@DependsOn("fireStoreService")
-class TokenFactory {
-    @Autowired
-    private lateinit var fireStoreService: FireStoreService
+class TokenFactory(private val defaultDataRepository: DefaultDataRepository) {
 
-    private lateinit var tokenProperties: TokenProperties
-
-    @PostConstruct
-    fun initialize(){
-        tokenProperties = fireStoreService.getTokenProperties()
-    }
+    val tokenProperties: TokenProperties
+        get() = defaultDataRepository.getTokenProperties()
 
     fun createTokens(userUUID: String, username: String): Tokens {
-        return Tokens(createAccessToken(userUUID, username), createRefreshToken(username, username))
+        return Tokens(createAccessToken(userUUID, username), createRefreshToken(userUUID, username))
     }
 
     private fun createAccessToken(userUUID: String, username: String): String {
         val claims = mutableMapOf("username" to username)
-        val now = ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(tokenProperties.expire.toLong())
+        //val expire = ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(tokenProperties.expire.toLong())
+        val expire = ZonedDateTime.now(ZoneOffset.UTC).plusSeconds(10)
 
         return Jwts.builder()
             .subject(userUUID)
             .issuer("https://api.karanda.kr/authentication")
             .claims(claims)
-            .signWith(SecretKeySpec(tokenProperties.secretKey.toByteArray(), tokenProperties.algorithm))
-            .expiration(Date.from(now.toInstant()))
+            .signWith(secretKey(tokenProperties.secretKey))
+            .expiration(Date.from(expire.toInstant()))
             .compact()
     }
 
@@ -57,7 +48,7 @@ class TokenFactory {
         return Jwts.builder()
             .issuer("https://api.karanda.kr/client")
             .claims(claims)
-            .signWith(SecretKeySpec(tokenProperties.platformKey.toByteArray(), tokenProperties.algorithm))
+            .signWith(secretKey(tokenProperties.platformKey))
             .expiration(Date.from(now.toInstant()))
             .compact()
     }
@@ -68,65 +59,83 @@ class TokenFactory {
         return Jwts.builder()
             .subject(userUUID)
             .issuer("https://api.karanda.kr/authentication")
-            .signWith(SecretKeySpec(tokenProperties.refreshKey.toByteArray(), tokenProperties.algorithm))
+            .signWith(secretKey(tokenProperties.refreshKey))
             .expiration(Date.from(now.toInstant()))
             .compact()
     }
 
     // This function is for access token
     fun getAuthentication(token: String): Authentication {
-        val key = SecretKeySpec(tokenProperties.secretKey.toByteArray(), tokenProperties.algorithm)
-        val payload = Jwts.parser().verifyWith(key).build().parseSignedClaims(token).payload
-        val user = User(userUUID = payload.subject, username = payload["username"].toString())
-        return UsernamePasswordAuthenticationToken(user, token)
+        val payload = Jwts
+            .parser()
+            .verifyWith(secretKey(tokenProperties.secretKey))
+            .build()
+            .parseSignedClaims(token)
+            .payload
+        val tokenClaims = TokenClaims(userUUID = payload.subject, username = payload["username"].toString())
+        return UsernamePasswordAuthenticationToken(tokenClaims, token)
     }
 
     fun getAuthenticationFromRefreshToken(token: String): Authentication {
-        val key = SecretKeySpec(tokenProperties.refreshKey.toByteArray(), tokenProperties.algorithm)
-        val payload = Jwts.parser().verifyWith(key).build().parseSignedClaims(token).payload
-        val user = User(userUUID = payload.subject, username = payload["username"].toString())
-        return UsernamePasswordAuthenticationToken(user, token)
+        val payload = Jwts
+            .parser()
+            .verifyWith(secretKey(tokenProperties.refreshKey))
+            .build()
+            .parseSignedClaims(token)
+            .payload
+        val tokenClaims = TokenClaims(userUUID = payload.subject, username = payload["username"].toString())
+        return UsernamePasswordAuthenticationToken(tokenClaims, token)
     }
 
     fun validateAccessToken(token: String): Boolean {
-        return validateToken(token, SecretKeySpec(tokenProperties.secretKey.toByteArray(), tokenProperties.algorithm))
+        return try {
+            validateToken(token, secretKey(tokenProperties.secretKey))
+        } catch (e: Exception) {
+            false
+        }
     }
 
     fun validateRefreshToken(token: String): Boolean {
-        return validateToken(token, SecretKeySpec(tokenProperties.refreshKey.toByteArray(), tokenProperties.algorithm))
+        try {
+            return validateToken(token, secretKey(tokenProperties.refreshKey))
+        } catch (e: ExpiredJwtException) {
+            println("Expired refresh token\n${e.message}")
+        }
+        return false
     }
 
     fun validateQualificationToken(token: String): Boolean {
-        return validateToken(token, SecretKeySpec(tokenProperties.platformKey.toByteArray(), tokenProperties.algorithm))
+        try {
+            return validateToken(token, secretKey(tokenProperties.platformKey))
+        } catch (e: ExpiredJwtException) {
+            println("Expired Qualification token\n${e.message}")
+        }
+        return false
     }
 
     private fun validateToken(token: String, key: SecretKeySpec): Boolean {
+        token.removePrefix("Bearer ")
+        var result = false
         try {
             Jwts.parser().verifyWith(key).build().parseSignedClaims(token)
-            return true
+            result = true
         } catch (e: SecurityException) {
             println("Invalid token\n${e.message}")
         } catch (e: MalformedKeyException) {
-            println(
-                "Invalid token\n" +
-                        "${e.message}"
-            )
+            println("Invalid token\n${e.message}")
         } catch (e: ExpiredJwtException) {
-            println(
-                "Expired token\n" +
-                        "${e.message}"
-            )
+            throw e
         } catch (e: UnsupportedJwtException) {
-            println(
-                "Unsupported Jwt token\n" +
-                        "${e.message}"
-            )
+            println("Unsupported Jwt token\n${e.message}")
         } catch (e: IllegalArgumentException) {
-            println(
-                "Invalid token\n" +
-                        "${e.message}"
-            )
+            println("Invalid token\n${e.message}")
+        } catch (e: Exception) {
+            println("Unsupported exception from {validateToken}\n${e.message}")
         }
-        return false
+        return result
+    }
+
+    fun secretKey(key: String): SecretKeySpec {
+        return SecretKeySpec(key.toByteArray(), tokenProperties.algorithm)
     }
 }
