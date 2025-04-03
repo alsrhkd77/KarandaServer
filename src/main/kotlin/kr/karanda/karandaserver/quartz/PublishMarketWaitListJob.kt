229f911e9156135da67c2942ba86550bd76df9c6
MinGwang Song <alsrhkd77@gmail.com>
2025-04-04 00:01:52 +0900

    미사용 코드 삭제

--- src/main/kotlin/kr/karanda/karandaserver/quartz/PublishMarketWaitListJob.kt
+++ src/main/kotlin/kr/karanda/karandaserver/quartz/PublishMarketWaitListJob.kt
@@ -1,12 +1,8 @@
 package kr.karanda.karandaserver.quartz
 
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.encodeToString
-import kr.karanda.karandaserver.enums.BDORegion
 import kr.karanda.karandaserver.service.TradeMarketService
 import org.quartz.JobExecutionContext
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.scheduling.quartz.QuartzJobBean
 import org.springframework.stereotype.Component
 
@@ -16,14 +12,7 @@ class PublishMarketWaitListJob: QuartzJobBean() {
     @Autowired
     private var tradeMarketService: TradeMarketService? = null
 
-    @Autowired
-    private var messagingTemplate: SimpMessagingTemplate? = null
-
     override fun executeInternal(context: JobExecutionContext) {
         tradeMarketService?.publishWaitList()
-        //TODO: 클라이언트 업데이트 후 제거해야함
-        tradeMarketService?.getWaitList(BDORegion.KR).let {
-            messagingTemplate?.convertAndSend("/live-data/trade-market/wait-list", Json.encodeToString(it))
-        }
     }
 }
