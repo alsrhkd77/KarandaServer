ab1fa28454eef3145fee199b1672c065d3ed7809
MinGwang Song <alsrhkd77@gmail.com>
2025-04-12 01:59:39 +0900

    디스코드 유저 데이터 초기값 업데이트

--- src/main/kotlin/kr/karanda/karandaserver/dto/DiscordUserData.kt
+++ src/main/kotlin/kr/karanda/karandaserver/dto/DiscordUserData.kt
@@ -1,10 +1,10 @@
 package kr.karanda.karandaserver.dto
 
 data class DiscordUserData(
-    val id: String,
-    val username: String,
-    val discriminator: String,
-    val avatar: String? = null,
-    val banner: String? = null,
-    val locale: String? = null,
+    var id: String,
+    var username: String = "unknown",
+    var discriminator: String,
+    var avatar: String? = null,
+    var banner: String? = null,
+    var locale: String? = null,
 )
