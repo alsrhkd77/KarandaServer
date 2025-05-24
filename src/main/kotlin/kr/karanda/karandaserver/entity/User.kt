4cfbe42c8132d7717bdb304e67efa29c9941d06b
MinGwang Song <alsrhkd77@gmail.com>
2025-05-24 16:19:35 +0900

    사용자 아바타 주소 설정 로직 수정

--- src/main/kotlin/kr/karanda/karandaserver/entity/User.kt
+++ src/main/kotlin/kr/karanda/karandaserver/entity/User.kt
@@ -28,13 +28,16 @@ class User(
 
     @OneToOne(fetch = FetchType.EAGER)
     var mainFamily: BDOFamily? = null,
+
+    @OneToMany(mappedBy = "owner", orphanRemoval = true)
+    val fcmSettings: List<UserFcmSettings> = mutableListOf(),
 ) {
     fun toUserDTO(): UserDTO {
         val embedded = discordDiscriminator.toIntOrNull()?.let { it % 6 } ?: 0
         return UserDTO(
             discordId = discordId,
             username = userName,
-            avatar = avatarHash?.apply { "https://cdn.discordapp.com/avatars/${discordId}/${this}.png" }
+            avatar = avatarHash?.let { "https://cdn.discordapp.com/avatars/${discordId}/${it}.png" }
                 ?: "https://cdn.discordapp.com/embed/avatars/${embedded}.png",
             mainFamily = mainFamily?.toDTO(),
         )
